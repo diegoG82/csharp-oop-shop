5e683e210f13eeb5566f0686735dad4c21041a1d
diegoG82 <diegogiardina@hotmail.it>
2023-09-08 16:13:05 +0200

    Aggiungere i file di progetto.

---
+++ csharp-oop-shop/Prodotto.cs
@@ -0,0 +1,47 @@
+﻿namespace csharp_oop_shop
+{
+    public class Prodotto
+    {
+        //ATTRIBUTI
+        private int codice;
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public double Prezzo { get; set; }
+        public int Iva { get; set; }
+
+        //STATI
+
+        //COSTRUTTORI
+
+        public Prodotto(string nome, string descrizione, double prezzo, int iva)
+        {
+            //codice per generare il codice del prodotto casuale di 8 cifre comprese tra 00000001 e 99999999
+            this.codice = new Random().Next(00000001, 99999999);
+            this.Nome = nome;
+            this.Descrizione = descrizione;
+            this.Prezzo = prezzo;
+            this.Iva = iva;
+        }
+
+        //GETTER
+
+        public int Codice
+        {
+            get { return codice; }
+        }
+        public double PrezzoBase
+        {
+            get { return Prezzo; }
+        }
+
+        public double PrezzoIvato
+
+        {
+            get { return Prezzo + (Prezzo / 100 * Iva); }
+        }
+
+    }
+}
+
+
+
---
+++ csharp-oop-shop/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+using csharp_oop_shop;
+
+Prodotto gunpla = new Prodotto("Modellino Robot", "Il Gunpla", 100, 22);
+
+Console.WriteLine($"il Codice: {gunpla.Codice} si riferisce al prodotto {gunpla.Nome} che è {gunpla.Descrizione} dal prezzo di {gunpla.Prezzo} e con iva costa {gunpla.PrezzoIvato}, l'iva attualmente e' al {gunpla.Iva}%");
